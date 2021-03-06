caebf5fa5d6809ed06839c965d4759c743443b1c
MiaMou <19845509@qq.com>
2021-03-06 15:34:49 +0800

    改造Notes.vue使其成为通用组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,22 @@
 <template>
     <Layout>
-        编辑标签
+        <div>
+            <Icon name="left"/>
+            <span>编辑标签</span> 
+        </div>
+        <Notes field-name="标签名" placeholder="请输入标签名"/>
     </Layout>
 </template>
 
 <script>
+    import Vue from 'vue';
     import tagListModel from '@/model/tagListModel';
-import Vue from 'vue';
     import { Component } from 'vue-property-decorator';
+    import Notes from "@/components/Money/Notes.vue";
 
-    @Component
+    @Component({
+        components: {Notes}
+    })
     export default class EditLabel extends Vue{
         created(){
             const id = this.$route.params.id;
