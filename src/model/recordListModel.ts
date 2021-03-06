23a854bf4fe2580f0f47da307bd4dee4680f561b
MiaMou <19845509@qq.com>
2021-03-06 18:05:33 +0800

    重新封装recordListModel

--- src/model/recordListModel.ts
+++ src/model/recordListModel.ts
@@ -1,17 +1,19 @@
 const localStorageKeyName = "recordList";
 const recordListModel = {
-  fetch() {
-    // 获取数据
-    return JSON.parse(
-      window.localStorage.getItem(localStorageKeyName) || "[]"
-    ) as RecordItem[];
-  },
-  save(data: RecordItem[]) {
-    // 保存数据
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
-  },
+  data: [] as RecordItem[],
   clone(data: RecordItem[] | RecordItem) {
     return JSON.parse(JSON.stringify(data));
+  },
+  fetch() {
+    // 获取数据
+    this.data = JSON.parse(
+      window.localStorage.getItem(localStorageKeyName) || "[]"
+    ) as RecordItem[];
+    return this.data;
+  },
+  save() {
+    // 保存数据
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   },
 };
 
